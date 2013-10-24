887996fa305d87dd14166c336d777c4571d49b21
Michael J Smith <msmit22@gmail.com>
2013-10-24 19:47:29 -0400

    Removed duplicate test

--- src/test/groovy/davescodeTest.groovy
+++
@@ -1,9 +0,0 @@
-import groovy.util.GroovyTestCase
-   
-class ExampleTest extends GroovyTestCase {
-    
-    void testFailingTest() {
-        Example myExample = new DaveExample()
-        assertTrue(myExample.fail())
-    }
-}
