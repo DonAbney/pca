923abdff27c70ea845240846f47dce1b2a7b9e2d
Amp Webmaster <webmaster@amppartners.org>
2013-10-24 23:10:48 -0400

    boyko's homework

--- src/test/groovy/DonkeyTest.groovy
+++ src/test/groovy/DonkeyTest.groovy
@@ -2,7 +2,11 @@ import groovy.util.GroovyTestCase
 
 class DonkeyTest extends GroovyTestCase {
 
-    def donkey = new Donkey()
+    def donkey
+
+    void setUp() {
+        donkey = new Donkey()
+    }
 
     void testGetDonkeyShouldReturnADonkey() {
         assertNotNull(donkey.get(1))
