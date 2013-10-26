6aa75a1968469f6ae746734e2f5fdc0bd8b97cce
DJ Daugherty <ddaugher@gmail.com>
2013-10-26 00:32:08 -0400

    refactor... i think final:

--- src/main/groovy/TrendUtils.groovy
+++ src/main/groovy/TrendUtils.groovy
@@ -1,11 +1,14 @@
 class TrendUtils {
 
+  def static COLLATE_SIZE = 2
+  def static COLLATE_STEP = 1
+
   def static findTrend(List list) {
 
   	if (list == null) return null 
   	if (list.isEmpty()) return null
 
-  	def collatedList = list.collate(2, 1)
+  	def collatedList = list.collate(COLLATE_SIZE, COLLATE_STEP)
 
   	def trend = 0.0d
   	collatedList.each {
@@ -17,7 +20,7 @@ class TrendUtils {
   }
 
   def private static gatherTrend(pair) {
-  		if (pair.size() != 2) return 0.0d
+  		if (pair.size() != COLLATE_SIZE) return 0.0d
 
   		if (bothElementsEqualZero(pair)) return 0.0d
 
