dc32521eb952bd68843ad9f7affe3ef1558d09b5
DJ Daugherty <ddaugher@gmail.com>
2013-10-25 23:28:33 -0400

    handling four numbers in the list

--- src/test/groovy/TrendUtilsTests.groovy
+++ src/test/groovy/TrendUtilsTests.groovy
@@ -30,4 +30,16 @@ class TrendUtilsTests extends GroovyTestCase {
     assert 0.2111111111111111 == TrendUtils.findTrend(l)
   }
 
+  void testShouldReturnProperPercentageWhenListOfThreeDifferentNumbersIncreasing() {
+    def l = [20.0d, 60.0d, 110.0d]
+
+    assert 2.8333333333333335 == TrendUtils.findTrend(l)
+  }
+
+  void testShouldReturnProperPercentageWhenListOfFourDifferentNumbersIncreasing() {
+    def l = [20.0d, 60.0d, 110.0d, 150.0d]
+
+    assert 3.1969696969696972 == TrendUtils.findTrend(l)
+  }
+
 }
