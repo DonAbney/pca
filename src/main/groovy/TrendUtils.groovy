97101fcbf3cc40d68c768adea5c11f753ddbcc8f
DJ Daugherty <ddaugher@gmail.com>
2013-10-25 23:59:48 -0400

    refactored... no longer required

--- src/main/groovy/TrendUtils.groovy
+++ src/main/groovy/TrendUtils.groovy
@@ -2,9 +2,10 @@ class TrendUtils {
 
   def static findTrend(List l) {
 
-  	if (l == null || l.isEmpty()) return null
+  	if (l == null) return null 
+  	if (l.isEmpty()) return null
 
-  	if (l.size() == 1) return 0.0d
+  	// if (l.size() == 1) return 0.0d
 
   	def collatedList = l.collate(2, 1)
 
