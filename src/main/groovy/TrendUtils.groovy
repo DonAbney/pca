a67466ed49f04eb3ffb0f6f8bdee39732e78be63
DJ Daugherty <ddaugher@gmail.com>
2013-10-26 00:10:45 -0400

    refactor for readability

--- src/main/groovy/TrendUtils.groovy
+++ src/main/groovy/TrendUtils.groovy
@@ -21,13 +21,17 @@ class TrendUtils {
 
   		if (bothElementsEqualZero(pair)) return 0.0d
 
-  		if (pair[0] == 0.0d && pair[1] > 0.0d) return 1.0d
+  		if (firstElementEqualZeroAndSecondGreaterThanZero(pair)) return 1.0d
 
   		(pair[1] - pair[0])/pair[0]
   }
 
   def private static bothElementsEqualZero(pair) {
-    return (pair[0] == 0.0d && pair[1] == 0.0d)
+    pair[0] == 0.0d && pair[1] == 0.0d
+  }
+
+  def private static firstElementEqualZeroAndSecondGreaterThanZero(pair) {
+  	pair[0] == 0.0d && pair[1] > 0.0d
   }
 
 }
