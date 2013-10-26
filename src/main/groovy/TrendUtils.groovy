4a68e5ce3d8b1f6d8150f16e2f9b8132140cda7b
DJ Daugherty <ddaugher@gmail.com>
2013-10-25 23:01:41 -0400

    adding test to handle incoming list of single value

--- src/main/groovy/TrendUtils.groovy
+++ src/main/groovy/TrendUtils.groovy
@@ -1,6 +1,10 @@
 class TrendUtils {
 
   def static findTrend(List l) {
+
+  	if (l == null || l.isEmpty()) return null
+
+  	if (l.size() == 1) return 0.0d
   }
 
 }
--- src/test/groovy/TrendUtilsTests.groovy
+++ src/test/groovy/TrendUtilsTests.groovy
@@ -6,4 +6,16 @@ class TrendUtilsTests extends GroovyTestCase {
     assert null == TrendUtils.findTrend(l)
   }
 
+  void testShouldReturnNullWhenListIsNull() {
+
+	assert null == TrendUtils.findTrend(null)
+
+  }
+
+  void testShouldReturnZeroWhenListOfOnlyOneNumber() {
+  	def l = [90.0d]
+
+  	assert 0.0d == TrendUtils.findTrend(l)
+  }
+
 }
