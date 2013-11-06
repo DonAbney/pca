43a197201e9448a6dfe000f82953a4b0c222ed8c
Pillar <pillar@James-Hoods-MacBook-Pro.local>
2013-11-05 19:34:41 -0500

    removing hello world tests from project

--- src/main/groovy/Dog.groovy
+++
@@ -1,5 +0,0 @@
-class Dog {
-    def bark() {
-        'woof'
-    }
-}
--- src/main/groovy/KenCode.groovy
+++
@@ -1,7 +0,0 @@
-// http://www.youtube.com/watch?v=FNmXGzgq0ls
-
-class KenCode {
-    def EvaluateMartialArt(art){
-        art == "AmeriDoTe" ? "$art is the one true path." : "$art is absolute bullshit."
-    }
-}
