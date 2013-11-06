4bf3ab84364add0fa69d0d73e212656e62a491e2
shieldscr <cshields@pillartechnology.com>
2013-11-05 20:12:33 -0500

    Began new test for displaying tweets

--- src/test/groovy/AcceptanceTest.groovy
+++ src/test/groovy/AcceptanceTest.groovy
@@ -1,11 +1,12 @@
 package test.groovy
 
+import groovy.util.XmlSlurper
 import main.groovy.Tweet
 import main.groovy.Twitter
 
 class AcceptanceTest extends GroovyTestCase {
 
-    void testWhenIRequestTweetsFromThePublicTimelineThenTheyAreDisplayed() {
+    void testWhenIRequestTweetsFromThePublicTimelineThenTheyContainInsertedTweets() {
     
         Tweet tweetOne = new Tweet('Don', 'This is a tweet from Don')
         Tweet tweetTwo = new Tweet('DJ', 'This is a tweet from DJ')
@@ -17,5 +18,18 @@ class AcceptanceTest extends GroovyTestCase {
 
         assert result.contains('<li>This is a tweet from Don</li>')
         assert result.contains('<li>This is a tweet from DJ</li>')
+    }
+
+
+    void testWhenIRequestTweetsFromThePublicTimelineThenTheyReturnedAsValidHtml() {
+        Tweet tweetOne = new Tweet('Don', 'This is a tweet from Don')
+        Tweet tweetTwo = new Tweet('DJ', 'This is a tweet from DJ')
+
+        def twitter = new Twitter()
+        twitter.setTweets([tweetOne, tweetTwo])
+
+        def result = twitter.displayPublicTimeline()
+
+        def rootNode = new XmlSlurper().parseText(result)
     }
 }
