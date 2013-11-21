d2c99f260051243ecc893538a07c7b7f963f7be8
henry <onyxorca@gmail.com>
2013-11-21 18:51:47 -0500

    updated test

--- src/test/groovy/com/pca/TwitterDisplayTest.groovy
+++ src/test/groovy/com/pca/TwitterDisplayTest.groovy
@@ -4,8 +4,9 @@ class TwitterDisplayTest extends GroovyTestCase {
 
     void testWhenATweetIsEnteredAnHtmlVersionIsReturned() {
         def twitterDisplay = new TwitterDisplay()
-        new Tweet(tweetHandle: "124", tweetText: "THIS IS MAH TWEET")
-        def expectedHtml = "<html><body><ul><li>THIS IS MAH TWEET</li></body></html>"
+        def tweet = new Tweet(tweetHandle: "124", tweetText: "THIS IS MAH TWEET")
+        def expectedHtml = "<div><p>124</p></div>" +
+                "<div><p>THIS IS MAH TWEET</p></div>"
 
         assertEquals(twitterDisplay.display(tweet), expectedHtml)
     }
