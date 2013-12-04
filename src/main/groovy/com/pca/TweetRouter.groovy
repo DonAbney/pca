4760538e030bb3f8a2bfec3f385eefcdc393ec30
Robert Juliano <rovjuvano@gmail.com>
2013-12-03 19:20:45 -0500

    Route whiteListed by Handle to whiteList

--- src/main/groovy/com/pca/TweetRouter.groovy
+++ src/main/groovy/com/pca/TweetRouter.groovy
@@ -2,7 +2,20 @@ package com.pca
 
 class TweetRouter {
     def greyList = new TweetList()
+    def whiteList = new TweetList()
+    def whiteListedHandles
     def addTweet(tweet) {
-        greyList.add(tweet)
+        if (isWhiteListed(tweet)) {
+            whiteList.add(tweet)
+        }
+        else {
+            greyList.add(tweet)
+        }
+    }
+
+    private isWhiteListed(tweet) {
+        whiteListedHandles.find {whiteListedHandle ->
+            whiteListedHandle.equals(tweet.tweetHandle)
+        }
     }
 }
--- src/test/groovy/com/pca/TweetRouterTest.groovy
+++ src/test/groovy/com/pca/TweetRouterTest.groovy
@@ -11,10 +11,34 @@ class TweetRouterTest extends GroovyTestCase {
         tweetRouter.addTweet(tweets[0]);
         tweetRouter.addTweet(tweets[1]);
 
-        def tweetList = tweetRouter.greyList
+        def greyList = tweetRouter.greyList
+        def whiteList = tweetRouter.whiteList
 
-        assert tweetList.size() == 2
-        assert tweetList.contains(tweets[0])
-        assert tweetList.contains(tweets[1])
+        assert greyList.size() == 2
+        assert greyList.contains(tweets[0])
+        assert greyList.contains(tweets[1])
+        assert whiteList.size() == 0
     }
+
+    void testShouldStoreWhiteListedTweets() {
+        def tweetRouter = new TweetRouter(whiteListedHandles: ['white'])
+        def tweets = [
+            new Tweet(tweetHandle: 'grey', tweetText: 'Text 2'),
+            new Tweet(tweetHandle: 'white', tweetText: 'Text 3'),
+        ]
+
+        tweetRouter.addTweet(tweets[0]);
+        tweetRouter.addTweet(tweets[1]);
+
+        def greyList = tweetRouter.greyList
+        def whiteList = tweetRouter.whiteList
+
+        assert greyList.size() == 1
+        assert whiteList.size() == 1
+        assert greyList.contains(tweets[0])
+        assert !greyList.contains(tweets[1])
+        assert !whiteList.contains(tweets[0])
+        assert whiteList.contains(tweets[1])
+    }
+
 }
