dc682ce1255a4148b4c0a9500e82a4294e770dc4
osofariu <ovi@sofariu.com>
2013-12-03 20:10:30 -0500

    added more tests for template handling

--- src/main/groovy/com/pca/TweetPresenter.groovy
+++ src/main/groovy/com/pca/TweetPresenter.groovy
@@ -2,11 +2,26 @@ package com.pca
 
 
 class TweetPresenter {
+    def templateFileName = "testTweetTemplate.html"
+
     def TweetPresenter() {
-        def htmlFile =  new File("tweetList.html")
-        if (htmlFile.exists()) {
-            htmlFile.delete()
-        }
-        htmlFile.createNewFile()
+        def templateText = initializeHtmlTemplateFile()
+        initializeDisplayHtmlFile(templateText)
     }
+
+    private String initializeHtmlTemplateFile() {
+        def templateFile = new File(templateFileName)
+        if (!templateFile.exists()) {
+            throw new GroovyRuntimeException()
+        }
+
+        templateFile.text
+    }
+
+    private void initializeDisplayHtmlFile(templateText) {
+        def htmlFile = new File("tweetList.html")
+        htmlFile.createNewFile()
+        htmlFile.write(templateText)
+    }
+
 }
--- src/test/groovy/com/pca/TweetPresenterTest.groovy
+++ src/test/groovy/com/pca/TweetPresenterTest.groovy
@@ -4,9 +4,11 @@ import groovy.util.GroovyTestCase;
 import org.junit.Test;
 
 public class TweetPresenterTest  extends GroovyTestCase  {
+    def testTemplateFileName = "testTweetTemplate.html"
 
     public void testsShouldCreateHTMLFileWhenClassCreatedAndNoFileExists() {
         def htmlFile = "tweetList.html"
+        createTemplate()
         new File(htmlFile).delete()
         new TweetPresenter()
         assert new File(htmlFile).exists()
@@ -14,12 +16,40 @@ public class TweetPresenterTest  extends GroovyTestCase  {
 
     public void testsShouldCreateNewHTMLFileWhenClassCreatedAndOldFileExists() {
         def htmlFile = "tweetList.html"
+        createTemplate()
         def testFile = new File(htmlFile)
         testFile.createNewFile()
         testFile << "previous content"
+
         new TweetPresenter()
+
         def finalFile = new File(htmlFile)
         assert finalFile.exists();
-        assert finalFile.size() == 0
+        assert finalFile.text != "previous content"
+    }
+
+    public void testsShouldThrowAnExceptionIfNoHtmlTemplateFileExists(){
+        new File("testTweetTemplate.html").delete()
+        shouldFail {
+            def presenter = new TweetPresenter()
+        }
+    }
+
+    public void testsShouldCopyTemplateFileContentIntoDisplayFile()  {
+        def templateFile = createTemplate()
+        templateFile << "Dummy template"
+
+        def presenter = new TweetPresenter()
+
+        def displayFile = new File("tweetList.html")
+        def displayContents = displayFile.text
+        assertEquals("Dummy template", displayContents)
+    }
+
+    private File createTemplate() {
+        def templateFile = new File("testTweetTemplate.html")
+        templateFile.createNewFile()
+        templateFile.write("")
+        templateFile
     }
 }
